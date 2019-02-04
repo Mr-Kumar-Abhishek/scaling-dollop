ff8270faafa21cd28355f9ebb16dad15f25191db
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-02-04 09:34:53 +0000

    log variables on click

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 /*global $ */
 
 function foo() {
+    console.clear();
     console.log("foo worked ... Yay ?");
     
-    var principle = $("#principle").val();
-    var buyingDate = $("#buyingDate").val();
-    var noYears = $("#noYears").val();
-    var interestYear = $("#interestYear").val();
-    var endAmount = $("#endAmount").val();
+    $("#calculate").click(function(){
+        
+        var principle = $("#principle").val();
+        var buyingDate = $("#buyingDate").val();
+        var noYears = $("#noYears").val();
+        var interestYear = $("#interestYear").val();
+        var endAmount = $("#endAmount").val();
     
-    console.log("principle: " + principle);
-    console.log("buying date: " + buyingDate);
-    console.log("Number of years: " + noYears);
-    console.log("Interest of Year:" + interestYear);
-    console.log("Returned Amount: " + endAmount);
-    
+        console.log("principle: " + principle);
+        console.log("buying date: " + buyingDate);
+        console.log("Number of years: " + noYears);
+        console.log("Interest of Year:" + interestYear);
+        console.log("Returned Amount: " + endAmount); 
+    });
 }
 
 $(foo());
