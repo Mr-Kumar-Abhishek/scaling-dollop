9a8e7cc1d99d48d15b67bb43b1c72e6621664662
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-02-04 08:58:55 +0000

    log all variables

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@
 
 function foo() {
     console.log("foo worked ... Yay ?");
+    
+    var principle = $("#principle").val();
+    var buyingDate = $("#buyingDate").val();
+    var noYears = $("#noYears").val();
+    var interestYear = $("#interestYear").val();
+    var endAmount = $("#endAmount").val();
+    
+    console.log("principle: " + principle);
+    console.log("buying date: " + buyingDate);
+    console.log("Number of years: " + noYears);
+    console.log("Interest of Year:" + interestYear);
+    console.log("Returned Amount: " + endAmount);
+    
 }
 
 $(foo());
