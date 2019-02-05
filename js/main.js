6bb5a77564eeb88544dc443a9d8ef9ad05e34ed0
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-02-05 18:12:31 +0530

    calculate rate 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 /*global $ */
 
+function r1_calculate(principle, interestYear){
+	var r1_result = interestYear/principle;
+	return r1_result;
+}
+
 function foo() {
     console.clear();
     console.log("foo worked ... Yay ?");
@@ -11,12 +16,14 @@ function foo() {
         var noYears = $("#noYears").val();
         var interestYear = $("#interestYear").val();
         var endAmount = $("#endAmount").val();
-    
+        var r1 = r1_calculate(principle, interestYear);
+            
         console.log("principle: " + principle);
         console.log("buying date: " + buyingDate);
         console.log("Number of years: " + noYears);
         console.log("Interest of Year:" + interestYear);
-        console.log("Returned Amount: " + endAmount); 
+        console.log("Returned Amount: " + endAmount);
+        console.log("Rate 1: " +  r1.toFixed(6));
     });
 }
 
