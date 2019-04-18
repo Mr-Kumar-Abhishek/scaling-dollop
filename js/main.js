03cb3b039cd83e45d9275858d133869aefbd8bdb
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-04-18 21:32:29 +0530

    set rough estimate of rate

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-/*global $ */
+/* global $ */
+
+function r2_calculate(r1, noYears, principle){
+        var r2_result = ((Math.pow((1/principle), (1/noYears))) - 1 + r1);
+        return r2_result;
+}
 
 function r1_calculate(principle, interestYear){
 	var r1_result = interestYear/principle;
@@ -17,13 +22,17 @@ function foo() {
         var interestYear = $("#interestYear").val();
         var endAmount = $("#endAmount").val();
         var r1 = r1_calculate(principle, interestYear);
-            
+        var r2 = r2_calculate(r1, noYears, principle);
+        
+        $("#rate").val(r2.toFixed(6));
+        
         console.log("principle: " + principle);
         console.log("buying date: " + buyingDate);
         console.log("Number of years: " + noYears);
         console.log("Interest of Year:" + interestYear);
         console.log("Returned Amount: " + endAmount);
         console.log("Rate 1: " +  r1.toFixed(6));
+        console.log("Rate 2: " + r2.toFixed(6));
     });
 }
 
